ae30d26d9e75a6b3af590dbaa48f102ce42e00c6
ZIBANi716 <eto52rion@gmail.com>
2026-05-01 13:31:23 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndlessNumericMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isrunning = true;
+            while (isrunning == true)
+            {
+                Console.WriteLine("Enter a number to check");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Error: not a number!");
+                    continue;
+                }
+                if (number < 0)
+                {
+                    Console.WriteLine("The number is negative!");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine("The number is positive!");
+                }
+                else
+                {
+                    Console.WriteLine("Zero value!");
+                }
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number is even!");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd!");
+                }
+                Console.WriteLine("Do you want to continue? y/n?");
+                string input = Console.ReadLine();
+                while (input != "y" && input != "n")
+                {
+                    Console.WriteLine("Invalid input! Enter y or n!");
+                    input = Console.ReadLine();
+                }
+                if (input == "n")
+                {
+                    isrunning = false;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Числовое меню с повтором")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Числовое меню с повтором")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("abd08b8c-6bb9-42f4-ba8b-2d582c8499f1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
